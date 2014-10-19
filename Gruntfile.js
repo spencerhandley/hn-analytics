b186bb1f38377d6008a37e7efaf6f0b0cb0448fa
spencer handley <spencerhandley@gmail.com>
2014-10-19 16:25:55 -0700

    update package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -185,8 +185,6 @@ module.exports = function (grunt) {
 
     // Automatically inject Bower components into the app
     wiredep: {
-      options: {
-      },
       app: {
         src: ['<%= yeoman.app %>/index.html'],
         ignorePath:  /\.\.\//
